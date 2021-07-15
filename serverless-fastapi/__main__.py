1bd74ed5b66cefb0c1d95e575d34872d1c679a9c
Anurag Kulkarni <anuragkulkarni4@gmail.com>
2021-07-15 12:41:21 -0400

    :sparkles: s3 and lambda successfully created

--- serverless-fastapi/__main__.py
+++ serverless-fastapi/__main__.py
@@ -7,7 +7,7 @@ import json
 
 region = aws.config.region
 
-custom_stage_name = 'serverless_fastapi_'
+custom_stage_name = 'serverless-fastapi-'
 
 ##################
 ## S3 bucket
@@ -15,11 +15,11 @@ custom_stage_name = 'serverless_fastapi_'
 
 # Create an S3 bucket to save lambda deployment package.
 
-serverless_fastapi_kms = aws.kms.Key("serverless_fastapi_kms",
+serverless_fastapi_kms = aws.kms.Key("serverless-fastapi-kms",
     description="KMS key 1",
     deletion_window_in_days=7)
-serverless_fastapi_bucket = aws.s3.Bucket("serverless_fastapi_bucket", acl="private")
-serverless_fastapi_bucket_object = aws.s3.BucketObject("serverless_fastapi_bucketObject",
+serverless_fastapi_bucket = aws.s3.Bucket("serverless-fastapi-bucket", acl="private")
+serverless_fastapi_bucket_object = aws.s3.BucketObject("serverless-fastapi-bucketObject",
     key="someobject",
     bucket=serverless_fastapi_bucket.id,
     source=pulumi.FileAsset("api/serverless_fastapi_deployment_package.zip"),
@@ -31,12 +31,11 @@ serverless_fastapi_bucket_object = aws.s3.BucketObject("serverless_fastapi_bucke
 
 # Create a Lambda function, using code from the `./app` folder.
 
-serverless_fastapi_lambda_func = aws.lambda_.Function("serverless_fastapi_lambda",
+serverless_fastapi_lambda_func = aws.lambda_.Function("serverless-fastapi-lambda",
     role=iam.lambda_role.arn,
-    runtime="python3.7",
+    runtime="python3.8",
     handler="main.handler",
-    s3_bucket="serverless_fastapi_bucket",
+    s3_bucket=serverless_fastapi_bucket.id,
     s3_key="someobject",
-    s3_object_version=serverless_fastapi_bucket_object    
-    })
-)
+    s3_object_version=serverless_fastapi_bucket_object.version_id    
+    )
