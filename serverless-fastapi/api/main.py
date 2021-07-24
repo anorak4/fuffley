ba587fec7e16a9bccbd5d3f5601d2e774b6e2d05
Anurag Kulkarni <anuragkulkarni4@gmail.com>
2021-07-24 09:38:01 -0400

    :sparkles: add web template

--- serverless-fastapi/api/main.py
+++ serverless-fastapi/api/main.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from v1.routers import router
 from mangum import Mangum
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI(title='Cryptocurrency API',
               description='API to track current prices and trading signals')
 app.include_router(router, prefix="/v1")
 
+templates = Jinja2Templates(directory="website")
 
-@app.get("/")
-def read_root():
-    return {"Hello Medium Reader": "from FastAPI & API Gateway"}
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {'request': request})
+    # return {"Hello Medium Reader": "from FastAPI & API Gateway"}
 
 
 # to make it work with Amazon Lambda, we create a handler object
